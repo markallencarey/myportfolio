1e0e1c51a333f8b0691170c69fef047731b729a1
Mark Carey <markallencarey@gmail.com>
2021-02-08 12:17:26 -0700

    iframe

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from "react-bootstrap";
+import PDF from '../assets/Resume.pdf'
+
+
 const Resume = (props) => {
 
   return (
     <Container className='Resume'>
       Resume.js
+      <iframe
+        title='resume'
+        style={{ width: '100%', height: '100%' }}
+        src={PDF}
+      />
     </Container>
   )
 }
