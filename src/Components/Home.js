bf1ec2d42dafd2acc461eb35950f830182daede3
Mark Carey <markallencarey@gmail.com>
2021-02-18 17:35:32 -0700

    initial commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -18,19 +18,19 @@ const Home = (props) => {
       <Row className='hero-title-row'>
         <Typewriter
           options={{
-            // loop: true,
+            loop: true,
             wrapperClassName: 'typewriter-title',
             cursorClassName: 'typewriter-cursor'
           }}
           onInit={typewriter => {
-            typewriter.typeString('Full Stack Web Developer.')
-              .pauseFor(1500)
+            typewriter.typeString('Full Stack Web Developer.<br>Music Producer. Guitarist.')
+              .pauseFor(5000)
               .start()
               .deleteAll()
-              .typeString('Music Producer. Guitarist.')
-              .pauseFor(1500)
-              .deleteAll()
-              .typeString('Full Stack Web Developer.<br>Music Producer. Guitarist.')
+              // .typeString('Music Producer. Guitarist.')
+              // .pauseFor(1500)
+              // .deleteAll()
+              // .typeString('Full Stack Web Developer.<br>Music Producer. Guitarist.')
           }}
         />
       </Row>
