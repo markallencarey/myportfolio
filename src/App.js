e459ec3e092ac4746732c7b75aa44ea8e22ce019
Mark Carey <markallencarey@gmail.com>
2021-02-21 23:53:28 -0700

    deleted pagescroller

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import Projects from './Components/Projects'
 import Resume from './Components/Resume'
 import Contact from './Components/Contact'
 import Stack from './Components/Stack'
-import ReactPageScroller from 'react-page-scroller'
 
 function App() {
   return (
     <div className="App">
-      {/* <ReactPageScroller> */}
         <Home />
         <Stack />
         <Projects />
         <Resume />
         <Contact />
-      {/* </ReactPageScroller> */}
     </div>
   );
 }
